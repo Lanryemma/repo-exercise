347a78d05912337b5f4ed1be60c81f1fd9f31bad
Lanryemma <lanryemma@gmail.com>
2024-06-05 18:30:19 +0100

    commit

--- JAVASCRIPT/REACT/TailwindProject/src/App.jsx
+++ JAVASCRIPT/REACT/TailwindProject/src/App.jsx
@@ -9,8 +9,8 @@
   return (
     <main className="relative">
         NAV 
-      <section className="xl:padding-1 wide:padding-a padding-b">
-
+      <section className="xl:padding-l wide:padding-r padding-b">
+        Hero
       </section>
     </main>
   )
